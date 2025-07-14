2be62a7d4d36781bc477884836df7c5fb4e764b9
Isaac Mertzenich <isaac.mertzenich@gmail.com>
2025-07-14 12:17:19 -0500

    useage comments added

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,4 +1,35 @@
 // src/components/Input/Input.tsx
+
+/**
+ * Input Component Usage
+ * 
+ * Import: import { Input } from 'cij-snipes';
+ * 
+ * Basic usage:
+ * <Input placeholder="Enter text..." />
+ * 
+ * With label:
+ * <Input label="Email" type="email" />
+ * 
+ * With error:
+ * <Input label="Password" error="Password is required" />
+ * 
+ * Custom styling:
+ * <Input className="w-full" label="Name" />
+ * 
+ * All standard HTML input attributes supported:
+ * <Input 
+ *   label="Username" 
+ *   type="text" 
+ *   placeholder="Enter username"
+ *   required
+ *   onChange={(e) => console.log(e.target.value)}
+ * />
+ * 
+ * Props: label?, error?, className?, plus all HTML input attributes
+ */
+
+
 import React from 'react';
 
 export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
--- src/components/Spinner/Spinner.tsx
+++ src/components/Spinner/Spinner.tsx
@@ -1,3 +1,32 @@
+/**
+ * Spinner Component Usage
+ * 
+ * Import: import { Spinner } from 'cij-snipes';
+ * 
+ * Basic usage:
+ * <Spinner />
+ * 
+ * Different sizes:
+ * <Spinner size="sm" />
+ * <Spinner size="md" />
+ * <Spinner size="lg" />
+ * 
+ * Different colors:
+ * <Spinner color="gray" />
+ * <Spinner color="blue" />
+ * <Spinner color="white" />
+ * 
+ * Combined options:
+ * <Spinner size="lg" color="blue" />
+ * 
+ * Custom styling:
+ * <Spinner className="mx-auto" />
+ * 
+ * Available sizes: sm, md (default), lg
+ * Available colors: gray (default), blue, white
+ * Accepts all standard HTML div attributes
+ */
+
 import React from "react";
 import clsx from "clsx";
 
